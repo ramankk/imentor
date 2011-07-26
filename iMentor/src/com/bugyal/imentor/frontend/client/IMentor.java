5153cb259c78ebc98735210a794c82601d89605e
raviteja <raviteja@kawanan.com>
2011-07-26 13:24:30 +0000

    Initial Facebook integration. trail 1

--- iMentor/src/com/bugyal/imentor/frontend/client/IMentor.java
+++ iMentor/src/com/bugyal/imentor/frontend/client/IMentor.java
@@ -1,21 +1,78 @@
 package com.bugyal.imentor.frontend.client;
 
-import com.bugyal.imentor.frontend.shared.OpportunityVO;
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.core.client.GWT;
-import com.google.gwt.core.client.JavaScriptObject;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.HorizontalPanel;
-import com.google.gwt.user.client.ui.ListBox;
-import com.google.gwt.user.client.ui.Panel;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
+import com.reveregroup.gwt.facebook4gwt.Facebook;
+import com.reveregroup.gwt.facebook4gwt.LoginButton;
+import com.reveregroup.gwt.facebook4gwt.events.FacebookLoginEvent;
+import com.reveregroup.gwt.facebook4gwt.events.FacebookLoginHandler;
+import com.reveregroup.gwt.facebook4gwt.user.UserField;
 
 
 public class IMentor implements EntryPoint {
 	
+	public LoginButton loginButton;
+	public Label statusLabel,userName;
+	
 	public void onModuleLoad() {
-		RootPanel.get("header").add(new HeaderWidget());
+		statusLabel = new Label();
+		userName = new Label();
+		Facebook.init("139577986094709");
+		loginButton = new LoginButton();
+		RootPanel.get().add(loginButton);
+		Window.alert("OK");
+		
+		Facebook.login(new AsyncCallback(){
+
+			@Override
+			public void onFailure(Throwable caught) {
+				Window.alert("failure");
+				
+			}
+
+			@Override
+			public void onSuccess(Object result) {
+				RootPanel.get("header").add(new HeaderWidget());
+				
+			}
+			
+		});
+	/*	Facebook.addLoginHandler(new FacebookLoginHandler(){
+
+			@Override
+			public void loginStatusChanged(FacebookLoginEvent event) {
+				if (event.isLoggedIn()){
+					Window.alert("logged in");
+							statusLabel.setText("Logged in");
+						
+				} else {
+					Window.alert("OK");
+					statusLabel.setText("No user logged in");
+				}	
+				
+			}
+			
+		});
+		
+		Facebook.APIClient().users_getLoggedInUser(new AsyncCallback() {
+		
+			@Override
+			public void onFailure(Throwable caught) {
+			    //userInfoPanel.setVisible(false);
+				RootPanel.get("header").add(new HeaderWidget());
+			  }
+				@Override
+				public void onSuccess(Object result) {
+					userName.setText(result.toString());
+					
+					//userInfoPanel.setVisible(true);
+					
+				}
+			}, UserField.NAME);*/
+		RootPanel.get().add(userName);
+		RootPanel.get().add(statusLabel);
 	}
 }
