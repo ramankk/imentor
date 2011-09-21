4a8c93a9c58955d523d3c791015b53ee3bef0450
sridhar.nalam <sridhar.nalam@kawanan.com>
2011-09-21 07:20:51 +0000

    added functionality to clear button in searchWidget. Zoom level is set to the user's selected zoom level in map widget.

--- iMentor/src/com/bugyal/imentor/frontend/client/SearchWidget.java
+++ iMentor/src/com/bugyal/imentor/frontend/client/SearchWidget.java
@@ -4,9 +4,7 @@ import java.util.ArrayList;
 
 import java.util.List;
 
-import com.bugyal.imentor.frontend.shared.ParticipantVO;
 import com.bugyal.imentor.frontend.shared.SearchResponse;
-import com.bugyal.imentor.frontend.shared.SearchResult;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ChangeEvent;
 import com.google.gwt.event.dom.client.ChangeHandler;
@@ -48,9 +46,13 @@ public class SearchWidget extends Composite implements ClickHandler,
 	public SearchWidget() {
 		service = (MentorServiceAsync) GWT.create(MentorService.class);
 		location = new TextArea();
+		location.setEnabled(false);
+		location.setSize("213px", "43px");
+		location.setText("Please use map");
 		location.setTitle("Please use map");
-		mapUI = new MapUI(true, location);		
-		subjectsSuggestWidget = new SubjectsSuggestWidget(new ArrayList<String>());
+		mapUI = new MapUI(false, location);
+		subjectsSuggestWidget = new SubjectsSuggestWidget(
+				new ArrayList<String>());
 		AsyncCallback<List<String>> callback = new AsyncCallback<List<String>>() {
 
 			@Override
@@ -65,27 +67,21 @@ public class SearchWidget extends Composite implements ClickHandler,
 
 		};
 		service.getSubjects(callback);
-		
+
 		VerticalPanel subjectsVertical = new VerticalPanel();
 		subjectsVertical.setHeight("80px");
 		subjectsVertical.add(new Label("Select Subjects"));
 		subjectsVertical.add(subjectsSuggestWidget);
 
-		location.setEnabled(false);
-		location.setSize("213px", "43px");
-		
 		VerticalPanel locationVertical = new VerticalPanel();
 		locationVertical.setHeight("80px");
 		Label l = new Label("Select Location:");
 		l.setHeight("20px");
 		locationVertical.add(l);
 		locationVertical.add(location);
-		
-		
-		
-		
+
 		VerticalPanel searchVertical = new VerticalPanel();
-		
+
 		listBox = new ListBox();
 		listBox.setTitle("Filters the results based on the option Selected");
 		listBox.addItem("All");
@@ -94,21 +90,22 @@ public class SearchWidget extends Composite implements ClickHandler,
 		HorizontalPanel dropdownHorizontal = new HorizontalPanel();
 		dropdownHorizontal.add(new Label("Search By:"));
 		dropdownHorizontal.add(listBox);
-		
+
 		HorizontalPanel btnHP = new HorizontalPanel();
 		searchBtn = new Button("Search");
 		searchBtn.addClickHandler(this);
 		clearBtn = new Button("Clear");
 		clearBtn.addClickHandler(this);
-		
+
 		btnHP.add(clearBtn);
 		btnHP.add(searchBtn);
-		
+
 		searchVertical.add(dropdownHorizontal);
 		searchVertical.add(btnHP);
-		searchVertical.setCellHorizontalAlignment(btnHP, HasHorizontalAlignment.ALIGN_RIGHT);
+		searchVertical.setCellHorizontalAlignment(btnHP,
+				HasHorizontalAlignment.ALIGN_RIGHT);
 		searchVertical.setHeight("80px");
-		
+
 		HorizontalPanel topHorizontal = new HorizontalPanel();
 		topHorizontal.add(subjectsVertical);
 		topHorizontal.add(locationVertical);
@@ -117,13 +114,13 @@ public class SearchWidget extends Composite implements ClickHandler,
 		tabPanel_1.add(topHorizontal, "Search");
 		tabPanel_1.selectTab(0);
 		tabPanel_1.setSize("700px", "150px");
-		switchView.add(mapUI,"Map View");
-		switchView.add(searchResultsWidget,"List View");
+		switchView.add(mapUI, "Map View");
+		switchView.add(searchResultsWidget, "List View");
 		switchView.setWidth("710px");
 		switchView.selectTab(0);
-		
+
 		VerticalPanel mainPanel = new VerticalPanel();
-		
+
 		mainPanel.add(tabPanel_1);
 		mainPanel.add(new HTML("<br>"));
 		mainPanel.add(switchView);
@@ -160,32 +157,36 @@ public class SearchWidget extends Composite implements ClickHandler,
 				location.setText("");// get from logged in user TODO Sundeep
 			}
 
-			service.filterList(locationData.getLatitude(), locationData
-					.getLongitude(), location.getText(), locationData
-					.getRadius(), hasSubs, needSubs,
+			service.filterList(locationData.getLatitude(),
+					locationData.getLongitude(), location.getText(),
+					locationData.getRadius(), hasSubs, needSubs,
 					new AsyncCallback<SearchResponse>() {
 
 						@Override
 						public void onFailure(Throwable caught) {
 							Window.alert("Unable to load");
+							showDefaultCursor();
 						}
 
 						@Override
 						public void onSuccess(SearchResponse result) {
 							sResponse = result;
 							showSearchResults(sResponse);
-
+							mapUI.setZoomLevel(mapUI.getZoomLevel());
 						}
 
 					});
+		} else if (event.getSource() == clearBtn) {
+			clearSearchWidget();
 		}
+
 	}
 
 	protected void showSearchResults(SearchResponse response) {
 		int type = listBox.getSelectedIndex();
 		showDefaultCursor();
-		
-		switch(type){
+
+		switch (type) {
 		case 0:
 			searchResultsWidget.setResults(response.getAllResults());
 			mapUI.addPartMarkers(type, response.getAllResults());
@@ -201,7 +202,7 @@ public class SearchWidget extends Composite implements ClickHandler,
 		default:
 			break;
 		}
-				
+
 	}
 
 	private void showDefaultCursor() {
@@ -214,6 +215,15 @@ public class SearchWidget extends Composite implements ClickHandler,
 
 	@Override
 	public void onChange(ChangeEvent event) {
+
+	}
+
+	public void clearSearchWidget() {
+		subjectsSuggestWidget.clearAll();
+		location.setText("Please, Use the Map");
+		listBox.setSelectedIndex(0);
+		mapUI.clear();
+		mapUI = new MapUI(false, location); 
 		
 	}
 
